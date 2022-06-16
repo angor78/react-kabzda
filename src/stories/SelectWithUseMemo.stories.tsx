d0574a207e13f2a812c7b6bf042ba4376aa801f3
Angor78 <angor78@gmail.com>
2022-06-16 12:20:52 +0300

    plugin ChakraUI

---
+++ src/stories/SelectWithUseMemo.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default {
+  title:'Select with hook useMemo'
+}
+
+export const Component = () => {
+
+  return(
+    <div>
+      <h2>Component</h2>
+    </div>
+  )
+}
