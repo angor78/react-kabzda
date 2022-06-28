ad7e6a1f2989937cfb2688139a633edc96c41934
Angor78 <angor78@gmail.com>
2022-06-28 13:14:49 +0300

    useState example2

--- src/stories/useState.stories.tsx
+++ src/stories/useState.stories.tsx
@@ -5,17 +5,28 @@ export default {
   title:'UseState demo'
 }
 
-function dificultCounting() {
-  console.log('render dificultCounting')
-  return 312312312138909
-}
-export const Example1 = ()=>{
+// function dificultCounting() {
+//   console.log('render dificultCounting')
+//   return 312312312138909
+// }
+// export const Example1 = ()=>{
+//   console.log('render Example1')
+//   const [counter,setCounter]=useState(dificultCounting)
+//   return(
+//     <div>
+//       {counter}
+//       <button onClick={()=>setCounter(counter+1)}>+</button>
+//     </div>
+//   )
+// }
+export const Example2 = ()=>{
   console.log('render Example1')
-  const [counter,setCounter]=useState(dificultCounting)
+  const [counter,setCounter]=useState(0)
+  const changer = (state:number)=>state+1
   return(
     <div>
       {counter}
-      <button onClick={()=>setCounter(counter+1)}>+</button>
+      <button onClick={()=>setCounter(changer)}>+</button>
     </div>
   )
 }
