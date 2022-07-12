fab7154d50ec352e7f16727744772d52dd35fecb
Angor78 <angor78@gmail.com>
2022-07-12 11:48:25 +0300

    useEffect timer clearInterval

--- src/stories/useEffectTimer.stories.tsx
+++ src/stories/useEffectTimer.stories.tsx
@@ -10,15 +10,17 @@ export const SimpleExample = () => {
   console.log('render SimpleExample')
   const [time, setTime] = useState(new Date())
   useEffect(() => {
-    setInterval(()=>{
+   const intID =  setInterval(() => {
       setTime(new Date())
-    },1000)
-
-  },[time])
+    }, 1000)
+    return ()=>{clearInterval(intID)}
+  }, [])
 
   return (
     <div>
-     {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      {time.getHours() < 10 ? '0' + time.getHours() : time.getHours()}:
+      {time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()}:
+      {time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds()}
     </div>
   )
 }
